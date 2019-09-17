23062705d3dab86cb1b2d3ba656948697bf2606a
eee27 <eee27go@outlook.com>
2019-09-17 23:25:55 +0800

    init

---
+++ src/test/java/com/eee27go/opms/OpmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eee27go.opms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
